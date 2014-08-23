9dd0e3773d72af8aadc0fdd4cb3e29e64b9fa234
Mairin Chesney <chesne14@msu.edu>
2014-08-23 13:14:51 -0400

    Skeleton files for resume.

--- server.rb
+++ server.rb
@@ -1,3 +1,12 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
+
+get '/' do
+	@content = JSON.parse File.read('content.json')
+	erb :index
+end
+
+get '/style.css' do
+	scss :style
+end
