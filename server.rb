78bae7646ff9b6be1f35add7f172da541724f125
mchesney <chesne14@msu.edu>
2014-12-28 16:19:22 +0000

    Lots of content updates

--- server.rb
+++ server.rb
@@ -2,9 +2,31 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'yaml'
 
+file_contents = YAML.load_file 'content.yml'
+
 get '/' do
-	@content = YAML.load_file 'content.yml'
+	@content = file_contents
 	erb :index
+end
+
+get '/experience' do
+	@content = file_contents
+	erb :experience
+end
+
+get '/education' do
+	@content = file_contents
+	erb :education
+end
+
+get '/skills' do
+	@content = file_contents
+	erb :skills
+end
+
+get '/awards' do
+	@content = file_contents
+	erb :awards
 end
 
 get '/style.css' do
