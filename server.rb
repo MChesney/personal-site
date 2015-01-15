4b737123ab710aa9bed17fd98299c4e5a41d6a86
Mairin Chesney <chesne14@msu.edu>
2015-01-15 11:41:45 -0800

    turned site way minial

--- server.rb
+++ server.rb
@@ -2,31 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'yaml'
 
-file_contents = YAML.load_file 'content.yml'
-
 get '/' do
-	@content = file_contents
 	erb :index
-end
-
-get '/resume' do
-	@content = file_contents
-	erb :resume
-end
-
-get '/projects' do
-	@content = file_contents
-	erb :projects
-end
-
-get '/photos' do
-	@content = file_contents
-	erb :photos
-end
-
-get '/contact' do
-	@content = file_contents
-	erb :contact
 end
 
 get '/style.css' do
